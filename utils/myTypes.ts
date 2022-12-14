26f6b02cd7b9a40954f66af9f29b5f44a7cd7533
higirobruce <higirobru@gmail.com>
2022-12-14 07:35:26 +0200

    User defined types

---
+++ utils/myTypes.ts
@@ -0,0 +1,27 @@
+
+export type IndustryType = "Agriculture; plantations,other rural sectors"
+| "Basic Metal Production"
+| "Chemical industries"
+| "Commerce"
+| "Construction"
+| "Education"
+| "Financial services, professional services"
+| "Food, drink, tobacco"
+| "Forestry, wood, pulp and paper"
+| "Health services"
+| "Hotels, tourism, catering"
+| "Mining (coal other mining)"
+| "Mechanical and electrical engineering"
+| "Media, culture, graphical"
+| "Oil and gas production, oil refining"
+| "Postal and telecommunications services"
+| "Public service"
+| "Shipping, ports, fisheries, inland waterways"
+| "Textiles, clothing, leather, footwear"
+| "Transport (including civil aviation, railways, road transport)"
+| "Transport equipment manufacturing"
+| "Utilities (water, gas, electricity)";
+export type LeadStatuses = 'New' | 'Contacted' | 'Working' | 'Qualified' | 'Unqualified';
+export type LeadSources = 'Advertisment' | 'Employee Referal' | 'External Referal' | 'In-Store' | 'On Site' | 'Other' | 'Social' | 'Trade Show' | 'Web' | 'Word of mouth';
+export type LeadRatings = 'Hot' | 'Warm' | 'Cold';
+export type AccountType = 'Analyst' | 'Competitor' | 'Customer' | 'Integrator' | 'Investor' | 'Partner';
